eb094c083e77a9bdb703e00dbc28c72c9817c539
Simon Vögele <simonvoegele@web.de>
2015-01-09 09:18:19 +0100

    exampels

---
+++ QuadVNH3SP30MotorDriver/examples/QuadMotor/QuadMotor.ino
@@ -0,0 +1,13 @@
+#include "QuadVNH3SP30MotorDriver.h"
+
+QuadVNH3SP30MotorDriver *md = new QuadVNH3SP30MotorDriver((unsigned char)22,(unsigned char)23,(unsigned char)2,(unsigned char)25,(unsigned char)24,(unsigned char)3,(unsigned char)27,(unsigned char)26,(unsigned char)4,(unsigned char)28,(unsigned char)29,(unsigned char)5);
+
+void setup() {
+  md->init();
+}
+
+void loop() {
+    md->setSpeeds(255,255,255,255);
+    delay(500);
+    md->setBrakes(0,0,0,0, 0); 
+}
